27badfc7f5aafee63f4edff15e34c774ad8a3de1
Junior <juniormbaya243@gmail.com>
2021-08-12 00:01:15 +0200

    add hero banner

--- components/Hero.js
+++ components/Hero.js
@@ -6,9 +6,17 @@ const HeroBanner = () => {
       <div className="object-contain object-center z-0 absolute">
         <Image src={HeroImage} alt="3 men with a box" />
       </div>
-      <div className="flex justify-center z-10 absolute border border-green-700 w-96 h-96 top-60 left-20">
-        <h1>Hello Text</h1>
+      <div className="flex flex-col items-center z-10 absolute border border-green-700 w-96 h-96 top-60 left-10 p-5">
+        <h1 className="text-6xl font-sans text-green-50">
+          Votre Spécialiste du déménagement
+        </h1>
+        <p>
+          Déménagement pour particuliers, solutions logistiques pour les
+          professionels et vente de carton et fourniture de déménagement.
+        </p>
+        <button className="font-space-grotesk">DEVIS</button>
       </div>
+      <div className="absolute z-10  border-8 border-green-700 w-96 h-96 top-60 left-10 p-5"></div>
     </div>
   );
 };
